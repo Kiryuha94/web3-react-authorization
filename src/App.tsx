ba6e74d9e77cc226f0c9c9d67dd43ee9a67a579a
Пользователь <kirillplusena@gmail.com>
2022-01-08 17:31:27 +0300

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { v4 as uuidv4 } from "uuid"
+export  const HomePage = () => {
+  return (
+    <Web3ReactProvider
+      getLibrary={(provider: any) => new Web3Provider(provider)}
+    >
+      <App />
+    </Web3ReactProvider>
+  );
+}
 
 function App() {
+  const { active, account, activate, library } = useWeb3React();
+  const [key, setKey] = useState<string>()
+  const [verifiResult, setVerifyResult] = useState(false)
+
+  useEffect(()=>{
+    activate(new InjectedConnector({}))
+  },[activate])
+
+  useEffect(()=>{
+    setKey(uuidv4())
+  },[active])
+
+const verifySignedMessage = (key:string):void => {
+console.log(key);
+setVerifyResult(key? true : false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div   >
+      <div>
+        {active ? (
+          <>
+            <div>
+              {account?.substr(0, 8)}
+            </div>
+            {!verifiResult ? (
+            <button
+              onClick={async () => {
+                const message = key;
+                const signature = await library
+                  .getSigner(account)
+                  .signMessage(message)
+                  .catch((error:string) => console.error(error));
+                  verifySignedMessage(signature);
+              }}
+            >
+              Sign In
+            </button>):(
+                <div>Authorization is successful</div>
+            )}
+          </>
+        ) : (
+          <div>
+           Please, install
+           <a href='https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn'> MetaMask</a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
